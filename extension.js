8949f0471909427a5b559c147bfef98055b6c764
Edu Wass <eduwass@gmail.com>
2024-01-08 11:22:45 +0100

    Remove unnecessary code in extension.js

--- extension.js
+++ extension.js
@@ -9,22 +9,9 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, "query-monitor-wordpress-snippets" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('query-monitor-wordpress-snippets.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Start typing \'qm\' in a PHP file and suggestions from Query Monitor WordPress Snippets should appear.');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
